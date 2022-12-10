70421ffb2ce04d86208dbfbe6199839fed41ca42
hideya <josyuya08@gmail.com>
2022-12-10 21:46:05 +0900

    Initial Commit

---
+++ DailyLife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyLife
+//
+//  Created by 始関秀弥 on 2022/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
